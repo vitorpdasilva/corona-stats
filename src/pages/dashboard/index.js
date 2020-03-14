c9e70de4652562dbe1159b3680698de8ab0b7bf7
Vitor Boccio <vitorboccio@gmail.com>
2020-03-14 16:07:57 +1100

    styling lite

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,67 +1,71 @@
 import React, { useState, useEffect } from 'react';
 import { Divider, Statistic } from 'semantic-ui-react'
-
-import { API_URL } from '../../constants';
+import { API_URL, COUNTRIES_LIST } from '../../constants';
+import DashboardWrapper from './styles';
 
 const Dashboard = () => {
   const [selectedCountry, setSelectedCountry] = useState();
+  const [message, setMessage] = useState();
+  const selectedCountryFullName = selectedCountry ? COUNTRIES_LIST.filter(i => i.sigla2 === selectedCountry) : null
   function useStats () {
     const [stats, setStats] = useState();
+    const [error, setError] = useState();
     useEffect(() => {
       async function fetchData() {
-        const res = await fetch(`${API_URL}/${selectedCountry ? 'countries/' : ''}${selectedCountry ? selectedCountry : ''}`).then(data => data.json());
-        setStats(res);
+        const res = await fetch(`${API_URL}/${selectedCountry ? 'countries/' : ''}${selectedCountry ? selectedCountry : ''}`)
+        .then(data => data.json())
+        .catch(err => {
+          setError(`Por enquanto nenhum caso registrado no país ${selectedCountry}`)
+          console.log(err);
+        })
+        if (!res.error) {
+          setMessage(null)
+          console.log(res);
+          setStats(res);
+        }
       }
       fetchData();
     }, [])
     return stats;
   }
 
-  function useCountries() {
-    const [countriesList, setCountriesList] = useState();
-    useEffect(() => {
-      async function fetchCountries() {
-        const res = await fetch(`${API_URL}/countries`).then(data => data.json());
-        setCountriesList(res)
-      }
-      fetchCountries();
-    }, []);
-    return countriesList;
-  }
-  
-
   function Stats() {
     const stats = useStats();
     if (!stats) return <p>Carregando...</p>
     return (
-      <Statistic.Group size="tiny">
-        <Statistic>
-          <Statistic.Value>{stats.confirmed.value}</Statistic.Value>
-          <Statistic.Label>Casos confirmados:</Statistic.Label>
-        </Statistic>
-        <Statistic>
-          <Statistic.Value>{stats.deaths.value}</Statistic.Value>
-          <Statistic.Label>Mortes:</Statistic.Label>
-        </Statistic>
-        <Statistic>
-          <Statistic.Value>{stats.recovered.value}</Statistic.Value>
-          <Statistic.Label>Recuperações:</Statistic.Label>
-        </Statistic>
-      </Statistic.Group>
+      <>
+        {message ? (
+          <h3>{message}</h3>
+        ) : (
+          <Statistic.Group size="tiny">
+          <Statistic>
+            <Statistic.Value>{stats.confirmed.value}</Statistic.Value>
+            <Statistic.Label>Confirmados</Statistic.Label>
+          </Statistic>
+          <Statistic>
+            <Statistic.Value>{stats.deaths.value}</Statistic.Value>
+            <Statistic.Label>Mortes</Statistic.Label>
+          </Statistic>
+          <Statistic>
+            <Statistic.Value>{stats.recovered.value}</Statistic.Value>
+            <Statistic.Label>Recuperações</Statistic.Label>
+          </Statistic>
+        </Statistic.Group>
+        )}
+      </>
     )
   }
 
   function FilterCountries() {
-    const list = useCountries();
     
-    if (!list) return <p>Carregando países...</p>
-    const { countries } = list;
+    if (!COUNTRIES_LIST) return <p>Carregando países...</p>
     return (
       <>
       <p>Countries</p>
       <select value={selectedCountry} onChange={e => setSelectedCountry(e.target.value)}>
-        {Object.keys(countries).map((i, index) => (
-          <option key={index} value={countries[i]}>{i}</option>
+        <option value="">GLOBAL</option>
+        {COUNTRIES_LIST.map((i, index) => (
+          <option key={index} value={i.sigla2}>{i.nome}</option>
         ))}
       </select>
       </>
@@ -69,15 +73,14 @@ const Dashboard = () => {
   }
   
   return (
-    <>
-      ESTATISTICAS {selectedCountry || "GLOBAIS"}:
+    <DashboardWrapper>
+      <h2 className="title">ESTATISTICAS {selectedCountry ? selectedCountryFullName[0].nome : "GLOBAIS"}</h2>
       <Divider hidden />
       <Stats />
       <Divider />
-      FILTRO DE PAISES: 
       <Divider hidden />
       <FilterCountries />
-    </>
+    </DashboardWrapper>
   )
 }
 
---
+++ src/pages/dashboard/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const DashboardWrapper = styled.div`
+  padding: 20px;
+  select {
+    height: 30px;
+    width: 100%;
+    border: 1px solid lightgray;
+  }
+  .title {
+    text-transform: uppercase;
+  }
+`;
+
+export default DashboardWrapper;
