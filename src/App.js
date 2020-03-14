8ceb8b64f9d1fe6d433ec7752322885d15869f0f
Vitor Boccio <vitorboccio@gmail.com>
2020-03-14 16:37:31 +1100

    removing unused files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { useContext, useReducer } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Context from './Context';
-import reducer from './reducer';
-
 import Dashboard from './pages/dashboard';
 
 const Root = () => {
-  const initialState = useContext(Context);
-  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <Router>
-      <Context.Provider value={{ state, dispatch }}>
-        <Switch>
-          <Route path="/" component={Dashboard} />
-        </Switch>
-      </Context.Provider>
+      <Switch>
+        <Route path="/" component={Dashboard} />
+      </Switch>
     </Router>
   )
 }
--- src/Context.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from 'react';
-
-const Context = createContext({
-  data: null,
-});
-
-export default Context;
