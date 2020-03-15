f8ebfaac8b7eece3d09a839fb6c7129f60c9d67a
Vitor Boccio <vitorboccio@gmail.com>
2020-03-15 18:25:16 +1100

    analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 import Header from './Header'; 
 import Footer from './Footer';
 import Dashboard from './pages/dashboard';
 import Daily from './pages/daily';
+
+const history = createBrowserHistory();
+
+ReactGA.initialize("UA-29845692-1");
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+
+
 
 const Root = () => {
   return (
