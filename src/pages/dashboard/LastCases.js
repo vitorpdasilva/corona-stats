4b5b7c6afa4128bd60e7d6aca0ffce379b549a60
Vitor Boccio <vitorboccio@gmail.com>
2020-04-05 11:06:54 +1000

    keeping data on store

--- src/pages/dashboard/LastCases.js
+++ src/pages/dashboard/LastCases.js
@@ -1,18 +1,13 @@
 import React, { useEffect, useState, useContext } from 'react';
-import _ from 'lodash';
-import { lightFormat, subDays } from 'date-fns';
 import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react';
 import Context from '../../context';
-import { API_URL } from '../../constants';
 import PlaceholderImage from '../../imgs/short-paragraph.png'
 import { ResponsiveContainer, ComposedChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 const LastCases = ({ country, timeRange }) => {
-  const [loading, setLoading] = useState(false);
   const { state: { dailyData } } = useContext(Context);
   const [chartData, setChartData] = useState([]);
   useEffect(() => {
-    console.log({ dailyData });
     const fetchLastCases = async () => {
       const entireData = [];
       if (!country) return <span></span>;
@@ -21,6 +16,7 @@ const LastCases = ({ country, timeRange }) => {
       dailyData.map(i => {
         const dataFilter = i.filter(entry => entry.countryRegion === testCountry) 
         entireData.push(dataFilter)
+        return true;
       });
         
       const formatedData = [];
@@ -30,7 +26,7 @@ const LastCases = ({ country, timeRange }) => {
         let recovered = 0;
         let active = 0;
         if (dailyEntry.length) {
-          dailyEntry.forEach((i, index) => {
+          dailyEntry.forEach(i => {
             confirmed += parseInt(i.confirmed);
             deaths += parseInt(i.deaths);
             recovered += parseInt(i.recovered);
@@ -51,7 +47,7 @@ const LastCases = ({ country, timeRange }) => {
   }, [country, dailyData]);
 
   if (!country) return <span></span>;
-  if (loading) {
+  if (dailyData.length < 1) {
     return (
       <Segment>
         <Dimmer active inverted>
@@ -61,43 +57,36 @@ const LastCases = ({ country, timeRange }) => {
       </Segment>
     )
   }
-  if (!loading && chartData) {
-    return (
-      <div style={{ width: '700px', maxWidth: '90%', height: 300, marginTop: '25px', marginLeft: '-20px' }}>
-        <ResponsiveContainer>
-          <ComposedChart data={chartData}>
-            <defs>
-              <linearGradient id="colorCases" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
-                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
-              </linearGradient>
-              <linearGradient id="colorDeaths" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="red" stopOpacity={0.8}/>
-                <stop offset="95%" stopColor="red" stopOpacity={0}/>
-              </linearGradient>
-              <linearGradient id="colorRecovered" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-              </linearGradient>
-            </defs>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="lastUpdate" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Area type="monotone" dataKey="newCases" stroke="#82ca9d" fillOpacity={1} fill="url(#colorCases)" />
-            <Area type="monotone" dataKey="newDeaths" stroke="red" fillOpacity={1} fill="url(#colorDeaths)" />
-            <Area type="monotone" dataKey="newRecovered" stroke="#8884d8" fillOpacity={1} fill="url(#colorRecovered)" />
-          </ComposedChart>
-        </ResponsiveContainer>
-      </div>
-    )
-  }
-  if (loading || !chartData) {
-    return (
-      <Loader size="small" />
-    )
-  }
+  return (
+    <div style={{ width: '700px', maxWidth: '90%', height: 300, marginTop: '25px', marginLeft: '-20px' }}>
+      <ResponsiveContainer>
+        <ComposedChart data={chartData}>
+          <defs>
+            <linearGradient id="colorCases" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8}/>
+              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0}/>
+            </linearGradient>
+            <linearGradient id="colorDeaths" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="red" stopOpacity={0.8}/>
+              <stop offset="95%" stopColor="red" stopOpacity={0}/>
+            </linearGradient>
+            <linearGradient id="colorRecovered" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+            </linearGradient>
+          </defs>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="lastUpdate" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Area type="monotone" dataKey="newCases" stroke="#82ca9d" fillOpacity={1} fill="url(#colorCases)" />
+          <Area type="monotone" dataKey="newDeaths" stroke="red" fillOpacity={1} fill="url(#colorDeaths)" />
+          <Area type="monotone" dataKey="newRecovered" stroke="#8884d8" fillOpacity={1} fill="url(#colorRecovered)" />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </div>
+  )
 }
 
 export default LastCases;
