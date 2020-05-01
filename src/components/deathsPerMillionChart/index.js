2de85d8944b4d6c71e453f85382e7aafad5fc160
Sebastian Fredriksson Bernholtz <sebefre@gmail.com>
2020-05-02 01:35:15 +1000

    generate chart color once on mount only
    Also had to refactor the useEffect functions into the useEffect callback. It was based on recommendation from https://reactjs.org/docs/hooks-faq.html#is-it-safe-to-omit-functions-from-the-list-of-dependencies
    
    I came to the above link after eslint started complaining and finding this: https://github.com/facebook/create-react-app/issues/6880

--- src/components/deathsPerMillionChart/index.js
+++ src/components/deathsPerMillionChart/index.js
@@ -8,7 +8,6 @@ import { API_URL } from '../../constants'; // Would be nice to set up absolut im
 import populationData from '../../data/processed-populations'
 import { getDisplayAlias, getPopulationAlias } from './countryTranslations'
 
-//! BUG: Keep the chart colors from changing. And maybe generate them from a color scheme. Probably good to move chartlines variable into the state and not regenerate on every rerender.
 //! TODO: Add the chart lines sorted by value. Most deaths per million first.
 //! FIXME: Cleanup aligning countries names in population and deaths. It's printing on each render.
 //! FIXME: Store data in global state with reducer and check if it needs to be updated.
@@ -23,7 +22,7 @@ const LOCATION_LEVEL = Object.freeze({
 })
 const TOP_COUNTRIES_NUMBER = 10
 const populationMissing = new Set()
-const searchableCountries = new Set()
+const searchableCountries = {}
 const initialDate = moment('2020-01-22') // This is the first date with source data on deaths
 
 export default function DeathsPerMillionChart() {
@@ -44,108 +43,106 @@ export default function DeathsPerMillionChart() {
       if (!topCountries.length) { setTopCountries(getTopCountries(deathsTimelineData)) }
       if (!alignTimelineOptions.length) { setAlignTimelineOptions(getAlignTimelineOptions(deathsTimelineData)) }
     }
-  }, [deathsTimelineData, topCountries, alignTimelineOptions]);
-
-  /* Functions */
-  async function getDeathsTimelineData() {
-    /* Main */
-    let currentDate = initialDate.clone()
-    const asyncOperations = []
-    const timelineData = []
-    while (!currentDate.isSame(moment(), 'day')) {
-      asyncOperations.push(addDataForDay(timelineData, currentDate.clone()))
-      currentDate.add(1, 'day')
-    }
-    await Promise.all(asyncOperations)
-    return timelineData
-
-    /* Helper functions */
-    async function addDataForDay(timelineData, date) {
-      const data = await fetch(`${API_URL}/daily/${date.format("M-D-YYYY")}`).then(data => data.json());
-      processDailyData(data, date, timelineData);
-    }
-
-    /**
-     * Add the daily data to the timeline. For each location add the deaths for that location to the city, province and
-     * country for that location. Multiple locations combine to give the resulting number for provinces and countries.
-     * @param {Object} dailyData 
-     * @param {moment} date 
-     * @param {Array} timelineData 
-     */
-    function processDailyData(dailyData, date, timelineData) {
-      // I'm assuming that there are no gaps in the data. So if a location has an entry on date A there will be an entry 
-      // on date B if B > A.
-      const dateIndex = date.diff(initialDate, "days")
-      timelineData[dateIndex] = { date: date.format("YYYY-MM-DD") }
-      dailyData.forEach(locationData => {
-        if (!locationData) { return }
-        const keyCountry  = `${getDisplayAlias(locationData.countryRegion) || ""}`
-        const keyProvince = `${locationData.provinceState || ""} - ${keyCountry}`
-        const keyCity     = `${locationData.admin2        || ""} - ${keyProvince}`
-        
-        const deaths = Number.parseInt(locationData.deaths) || 0
-        const addToProcessedDataLocal = (key, level) => addDeathsToProcessedData(timelineData, key, locationData, deaths, date, level) // Bind data that will be used in all calls to the function for the current location (ie for city, province and country)
-
-        if (locationData.admin2) { addToProcessedDataLocal(keyCity, LOCATION_LEVEL.CITY) }
-        if (locationData.provinceState) { addToProcessedDataLocal(keyProvince, LOCATION_LEVEL.PROVINCE) }
-        if (locationData.countryRegion) { addToProcessedDataLocal(keyCountry, LOCATION_LEVEL.COUNTRY) }
-      })
-
-      // Change from absolute deaths to deaths per million
-      Object.entries(timelineData[dateIndex]).forEach(entry => {
-        if (entry[0] === "date") { return }
-        const [ location, deaths ] = entry
-        const population = populationData[getPopulationAlias(location)]
-        if (!population) {
-          if (!location.includes(' - ')) { populationMissing.add(location) } // ' - ' is included if it's lower level than country.
-          return delete timelineData[dateIndex][location]
-        }
-        timelineData[dateIndex][location] = Math.round(deaths / population.Value * 1_000_000)
-        searchableCountries.add(location)
-      })
-    }
-
-    function addDeathsToProcessedData(processedData, locationKey, newData, deaths, date, locationLevel) {
-      const dateIndex = date.diff(initialDate, "days")
-      const existingDateEntry = processedData[dateIndex]
-      const existingLocationEntry = existingDateEntry && processedData[dateIndex][locationKey]
-
-      processedData[dateIndex][locationKey] = existingLocationEntry
-        ? existingLocationEntry + deaths
-        : deaths
-    }
-  }
-
-  function getTopCountries(deathsTimelineData) {
-    const finalDayData = getFinalDayData()
-    return Object.entries(finalDayData).sort((a, b) => b[1] - a[1]).slice(0,TOP_COUNTRIES_NUMBER).reduce((res,item) => { res.push(item[0]); return res }, [])// Take final day data minus the data key, sort by values, take top countries, and reduce the names to an array.
-
-    /** Get the the data for the last day with at least as much data as the top countries number */
-    function getFinalDayData() {
-      let finalDayData = {}
-      let i = 1
-      while (Object.keys(finalDayData).length < TOP_COUNTRIES_NUMBER + 1) { // + 1 to account for 'date' key-value
-        finalDayData = deathsTimelineData[deathsTimelineData.length - i++]
+    
+    /* Functions */
+    async function getDeathsTimelineData() {
+      /* Main */
+      let currentDate = initialDate.clone()
+      const asyncOperations = []
+      const timelineData = []
+      while (!currentDate.isSame(moment(), 'day')) {
+        asyncOperations.push(addDataForDay(timelineData, currentDate.clone()))
+        currentDate.add(1, 'day')
       }
-      return _.omit(finalDayData, 'date')
-    }
-  }
+      await Promise.all(asyncOperations)
+      return timelineData
 
-  function getAlignTimelineOptions(deathsTimelineData) {
-    const max = _.max(deathsTimelineData.map(entry => {
-      const entryWithoutDateKey = _.omit(entry, 'date')
-      return _.max(Object.values(entryWithoutDateKey))
-    }))
-    const alignTimelineOptions = [ { key: 'None', text: 'None' } ]
-    for (let i = 0; i <= max; i++ ) {
-      alignTimelineOptions.push({ key: i, text: i, value: i })
-    }
-    return alignTimelineOptions
-  }
+      /* Helper functions */
+      async function addDataForDay(timelineData, date) {
+          const data = await fetch(`${API_URL}/daily/${date.format("M-D-YYYY")}`).then(data => data.json());
+          processDailyData(data, date, timelineData);
+        }
 
-  function getRandomColour() {
-    return '#'+ Math.round(Math.random() * 0xffffff).toString(16).padStart(6,'0')
-  }
+      /**
+       * Add the daily data to the timeline. For each location add the deaths for that location to the city, province and
+       * country for that location. Multiple locations combine to give the resulting number for provinces and countries.
+       * @param {Object} dailyData 
+       * @param {moment} date 
+       * @param {Array} timelineData 
+       */
+      function processDailyData(dailyData, date, timelineData) {
+        // I'm assuming that there are no gaps in the data. So if a location has an entry on date A there will be an entry 
+        // on date B if B > A.
+        const dateIndex = date.diff(initialDate, "days")
+        timelineData[dateIndex] = { date: date.format("YYYY-MM-DD") }
+        dailyData.forEach(locationData => {
+          if (!locationData) { return }
+          const keyCountry  = `${getDisplayAlias(locationData.countryRegion) || ""}`
+          const keyProvince = `${locationData.provinceState || ""} - ${keyCountry}`
+          const keyCity     = `${locationData.admin2        || ""} - ${keyProvince}`
+          
+          const deaths = Number.parseInt(locationData.deaths) || 0
+          const addToProcessedDataLocal = (key, level) => addDeathsToProcessedData(timelineData, key, locationData, deaths, date, level) // Bind data that will be used in all calls to the function for the current location (ie for city, province and country)
+
+          if (locationData.admin2) { addToProcessedDataLocal(keyCity, LOCATION_LEVEL.CITY) }
+          if (locationData.provinceState) { addToProcessedDataLocal(keyProvince, LOCATION_LEVEL.PROVINCE) }
+          if (locationData.countryRegion) { addToProcessedDataLocal(keyCountry, LOCATION_LEVEL.COUNTRY) }
+        })
+
+        // Change from absolute deaths to deaths per million
+        Object.entries(timelineData[dateIndex]).forEach(entry => {
+          if (entry[0] === "date") { return }
+          const [ location, deaths ] = entry
+          const population = populationData[getPopulationAlias(location)]
+          if (!population) {
+            if (!location.includes(' - ')) { populationMissing.add(location) } // ' - ' is included if it's lower level than country.
+            return delete timelineData[dateIndex][location]
+          }
+          timelineData[dateIndex][location] = Math.round(deaths / population.Value * 1_000_000)
+          searchableCountries[location] = getRandomColour()
+
+          function getRandomColour() {
+            return '#'+ Math.round(Math.random() * 0xffffff).toString(16).padStart(6,'0')
+          }
+        })
+      }
+
+      function addDeathsToProcessedData(processedData, locationKey, newData, deaths, date, locationLevel) {
+        const dateIndex = date.diff(initialDate, "days")
+        const existingDateEntry = processedData[dateIndex]
+        const existingLocationEntry = existingDateEntry && processedData[dateIndex][locationKey]
+
+        processedData[dateIndex][locationKey] = existingLocationEntry
+          ? existingLocationEntry + deaths
+          : deaths
+      }
+    }
+    function getTopCountries(deathsTimelineData) {
+      const finalDayData = getFinalDayData()
+      return Object.entries(finalDayData).sort((a, b) => b[1] - a[1]).slice(0,TOP_COUNTRIES_NUMBER).reduce((res,item) => { res.push(item[0]); return res }, [])// Take final day data minus the data key, sort by values, take top countries, and reduce the names to an array.
+  
+      /** Get the the data for the last day with at least as much data as the top countries number */
+      function getFinalDayData() {
+        let finalDayData = {}
+        let i = 1
+        while (Object.keys(finalDayData).length < TOP_COUNTRIES_NUMBER + 1) { // + 1 to account for 'date' key-value
+          finalDayData = deathsTimelineData[deathsTimelineData.length - i++]
+        }
+        return _.omit(finalDayData, 'date')
+      }
+    }
+    function getAlignTimelineOptions(deathsTimelineData) {
+      const max = _.max(deathsTimelineData.map(entry => {
+        const entryWithoutDateKey = _.omit(entry, 'date')
+        return _.max(Object.values(entryWithoutDateKey))
+      }))
+      const alignTimelineOptions = [ { key: 'None', text: 'None' } ]
+      for (let i = 0; i <= max; i++ ) {
+        alignTimelineOptions.push({ key: i, text: i, value: i })
+      }
+      return alignTimelineOptions
+    }
+  }, [deathsTimelineData, topCountries, alignTimelineOptions]);
 
   function updateSelectedCountries(selectedCountries) {
     setSelectedCountries(selectedCountries)
@@ -154,7 +151,7 @@ export default function DeathsPerMillionChart() {
         <Line
           type="monotone"
           dataKey={getDisplayAlias(country)}
-          stroke={getRandomColour()}
+          stroke={searchableCountries[getDisplayAlias(country)]}
           dot={false}
           key={country}
         />
@@ -189,10 +186,10 @@ export default function DeathsPerMillionChart() {
     setAdjustedTimelineData(adjustedTimelineData)
   }
 
-  console.log(_.difference(Object.keys(populationData).map(country => country),[...searchableCountries].map(country => getPopulationAlias(country)) ))
+  console.log(_.difference(Object.keys(populationData).map(country => country), Object.keys(searchableCountries).map(country => getPopulationAlias(country)) ))
   console.log(populationMissing)
 
-  const searchOptions = [...searchableCountries].sort().map(country => ({
+  const searchOptions = Object.keys(searchableCountries).sort().map(country => ({
     key: country,
     text: country,
     value: country,
